24bb8c5b1a9a68a4e0fd747db0136a54d85c68cc
JUAN MENDIOLA ARGUELLES <juan.mendiola@cecytlax.edu.mx>
2026-09-05 22:14:49 -0600

    Delete config[1].js

--- config[1].js
+++
@@ -1,7 +0,0 @@
-// VALÓRATE CON S-CIENCIA 4.0
-// Coloca únicamente la URL del proyecto y la clave pública ANON de Supabase.
-// NUNCA coloques una clave service_role en este archivo.
-window.SC_CONFIG = {
-  SUPABASE_URL: "PEGA_AQUI_TU_URL_DE_SUPABASE",
-  SUPABASE_ANON_KEY: "PEGA_AQUI_TU_CLAVE_ANON_PUBLICA"
-};
